671bcb3f0a48fa289b9025ff98706fa1c5046bc3
mcarfagno <m.carfagno@outlook.it>
2019-07-26 16:35:29 +0100

    figure updates, removed uused files

--- IK_server_JT.py
+++ IK_server_JT.py
@@ -6,110 +6,145 @@ from kuka_arm.srv import *
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from geometry_msgs.msg import Pose
 from sympy import *
-from mpmath import radians
+import numpy as np
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received {} poses from the plan".format(len(req.poses)))
     if len(req.poses)<1:
         return -1
     else:
+
+        #Define  DH param symbols
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0,alpha1,alpha2,alpha3,alpha4,alpha5,alpha6 = symbols('alpha0:7')
+
+        #Define Joint Variables symbols
+        q1, q2, q3, q4, q5, q6, q7 =symbols('q1:8')
+
+        #Modified UR5 DH Parameters Table
+        DH_table = { alpha0: 0,      a0: 0,       d1: 0.089159, q1: q1, #Shoulder Pan Joint
+                     alpha1: pi/2,   a1: 0,        d2: 0,       q2: q2, #Shoulder Lift Joint
+                     alpha2: 0,      a2: -0.42500, d3: 0,       q3: q3, #Elbow Joint
+                     alpha3: 0,      a3: -0.39225, d4: 0.10915, q4: q4, #Wirst 1 Joint
+                     alpha4: pi/2,   a4: 0,        d5: 0.09465, q5: q5, #Wirst 2 Joint
+                     alpha5: -pi/2,  a5: 0,        d6: 0.0823,  q6: q6, #Wirst 3 Joint
+                     alpha6: 0,      a6: 0,        d7: 0.085,   q7: 0}  #Gripper Offset
+
+        #Define Homogeneous Transform Matrix from DH parameters
+        def homogeneous_transform(q, d, a, alpha):
+
+            T = Matrix([[cos(q)           ,           -sin(q),           0,             a],
+                        [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
+                        [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
+                        [                0,                 0,           0,             1]])
+            return T
+
+        #Create individual transformation matrices
+        T0_1  = homogeneous_transform(q1, d1, a0, alpha0).subs(DH_table)
+        T1_2  = homogeneous_transform(q2, d2, a1, alpha1).subs(DH_table)
+        T2_3  = homogeneous_transform(q3, d3, a2, alpha2).subs(DH_table)
+        T3_4  = homogeneous_transform(q4, d4, a3, alpha3).subs(DH_table)
+        T4_5  = homogeneous_transform(q5, d5, a4, alpha4).subs(DH_table)
+        T5_6  = homogeneous_transform(q6, d6, a5, alpha5).subs(DH_table)
+        T6_EE = homogeneous_transform(q7, d7, a6, alpha6).subs(DH_table)
+
+        #Compute FK
+        T0_EE = T0_1*T1_2*T2_3*T3_4*T4_5*T5_6*T6_EE
+
+        angle = symbols('angle')
+
+        R_y = Matrix([[  cos(angle), 0, sin(angle)],
+                      [           0, 1,          0],
+                      [ -sin(angle), 0, cos(angle)]])
+
+        R_z = Matrix([[ cos(angle), -sin(angle),  0],
+                      [ sin(angle),  cos(angle),  0],
+                      [          0,           0,  1]])
+
+        #Gazebo to DH base link orientation offset
+        R_BASE = R_z.subs('angle',pi)
+        T_BASE = R_BASE.col_insert(3, Matrix([0, 0, 0]))
+        T_BASE = T_BASE.row_insert(3, Matrix([[0, 0, 0, 1]]))
+
+        #DH to Gazebo EE orientation offset
+        R_CORR = R_z.subs('angle',pi/2)*R_y.subs('angle',-pi/2)
+        T_CORR = R_CORR.col_insert(3, Matrix([0, 0, 0]))
+        T_CORR = T_CORR.row_insert(3, Matrix([[0, 0, 0, 1]]))
+
+        T0_EE_GAZEBO = T_BASE*T0_EE*T_CORR
+
+        #Compute EE Pose Vector
+        X = zeros(6,1)
+        X[0:3,0] = T[0:3,3]
+        X[3,0] = atan2(T0_EE_GAZEBO[2,1],T0_EE_GAZEBO[2,2]) #Roll
+        X[4,0] = atan2(-T0_EE_GAZEBO[2,0], sqrt(T0_EE_GAZEBO[0,0]*T0_EE_GAZEBO[0,0] + T0_EE_GAZEBO[1,0]*T0_EE_GAZEBO[1,0])) #Pitch
+        X[5,0] = atan2(T0_EE_GAZEBO[1,0],T0_EE_GAZEBO[0,0]) #YAW
+        x=lambdify([q1,q2,q3,q4,q5,q6],X,"numpy")
+
+        #Compute Analytical Jacobian
+        J = zeros(6,6)
+        J[:,0] = diff(X,q1)
+        J[:,1] = diff(X,q2)
+        J[:,2] = diff(X,q3)
+        J[:,3] = diff(X,q4)
+        J[:,4] = diff(X,q5)
+        J[:,5] = diff(X,q6)
+        j=lambdify([q1,q2,q3,q4,q5,q6],J,"numpy")
+
         #Initialize response
         joint_trajectory_list =[]
-        for x in xrange(0, len(req.poses)):
-            '''
-            IK Steps:
-            -
-            -
-            -
-            '''
+
+        for i in xrange(0, len(req.poses)):
+
             joint_trajectory_point = JointTrajectoryPoint()
 
-            #Define  DH param symbols
-            d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
-            a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
-            alpha0,alpha1,alpha2,alpha3,alpha4,alpha5,alpha6 = symbols('alpha0:7')
+            #Extract EE goal pose
+            px = req.poses[i].position.x
+            py = req.poses[i].position.y
+            pz = req.poses[i].position.z
 
-            #Define Joint Variables symbols
-            q1, q2, q3, q4, q5, q6, q7 =symbols('q1:8')
+            (roll, pitch, yaw) =tf.transformations.euler_from_quaternion([req.poses[i].orientation.x,
+                                                                          req.poses[i].orientation.y,
+                                                                          req.poses[i].orientation.z,
+                                                                          req.poses[i].orientation.w,])
 
-            #Modified DH Parameters Table
-            DH_table = {alpha0:     0, a0:      0, d1:  0.75, q1:      q1,
-                        alpha1: -pi/2, a1:   0.35, d2:     0, q2: q2-pi/2,
-                        alpha2:     0, a2:   1.25, d3:     0, q3:      q3,
-                        alpha3: -pi/2, a3: -0.054, d4:   1.5, q4:      q4,
-                        alpha4:  pi/2, a4:      0, d5:     0, q5:      q5,
-                        alpha5: -pi/2, a5:      0, d6:     0, q6:      q6,
-                        alpha6:     0, a6:      0, d7: 0.303, q7:       0}
+            x_goal = np.array([[px,py,pz,roll,pitch,yaw]]).T
 
-            #Define Homogeneous Transform Matrix from DH parameters
-            def homogeneous_transform(q, d, a, alpha):
+            #Initialize
+            '''
+            TODO: get theta values from Gazebo instead
+            joint_state_msg = rospy.wait_for_message(topic, topic_type, timeout=None)
+            '''
+            q = np.array([[0,0,0,0,0,0]]).T
+            x_ee=x(q[0],q[1],q[2],q[3],q[4],q[5]).astype(np.float64)
+            x_ee=np.squeeze(x_ee, axis=2)
 
-                T = Matrix([[cos(q)           ,           -sin(q),           0,             a],
-                            [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-                            [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-                            [                0,                 0,           0,             1]])
-                return T
+            #ompute Error
+            delta_x=x_goal-x_ee
+            delta_x[3]=( delta_x[3] + np.pi) % (2 * np.pi ) - np.pi
+            delta_x[4]=( delta_x[4] + np.pi) % (2 * np.pi ) - np.pi
+            delta_x[5]=( delta_x[5] + np.pi) % (2 * np.pi ) - np.pi
 
-            #Create individual transformation matrices
-            T0_1  = homogeneous_transform(q1, d1, a0, alpha0).subs(DH_table)
-            T1_2  = homogeneous_transform(q2, d2, a1, alpha1).subs(DH_table)
-            T2_3  = homogeneous_transform(q3, d3, a2, alpha2).subs(DH_table)
-            T3_4  = homogeneous_transform(q4, d4, a3, alpha3).subs(DH_table)
-            T4_5  = homogeneous_transform(q5, d5, a4, alpha4).subs(DH_table)
-            T5_6  = homogeneous_transform(q6, d6, a5, alpha5).subs(DH_table)
-            T6_EE = homogeneous_transform(q7, d7, a6, alpha6).subs(DH_table)
+            while(np.sum(np.abs(delta_x)) > 0.01):
 
-            #Compute FK
-            T0_EE = T0_1*T1_2*T2_3*T3_4*T4_5*T5_6*T6_EE
+                ja=(j(q[0],q[1],q[2],q[3],q[4],q[5])).astype(np.float64)
+                delta_q = 0.25*(ja.T).dot(delta_x)
+                q=q+delta_q
 
-            #Extract EE position and orientation from request
-            px = req.poses[x].position.x
-            py = req.poses[x].position.y
-            pz = req.poses[x].position.z
+                x_ee=x(q[0],q[1],q[2],q[3],q[4],q[5]).astype(np.float64)
+                x_ee=np.squeeze(x_ee, axis=2)
 
-            (roll, pitch, yaw) =tf.transformations.euler_from_quaternion([req.poses[x].orientation.x,
-                                                                          req.poses[x].orientation.y,
-                                                                          req.poses[x].orientation.z,
-                                                                          req.poses[x].orientation.w,])
+                delta_x=x_goal-x_ee
+                delta_x[3]=( delta_x[3] + np.pi) % (2 * np.pi ) - np.pi
+                delta_x[4]=( delta_x[4] + np.pi) % (2 * np.pi ) - np.pi
+                delta_x[5]=( delta_x[5] + np.pi) % (2 * np.pi ) - np.pi
 
-            #Compute EE rotation Matrix using RPY Euler angles
-            r,p,y = symbols('r,p,y')
-
-            ROT_x = Matrix([[ 1,      0,       0],
-                            [ 0, cos(r), -sin(r)],
-                            [ 0, sin(r),  cos(r)]])
-
-            ROT_y = Matrix([[  cos(p), 0, sin(p)],
-                            [       0, 1,      0],
-                            [ -sin(p), 0, cos(p)]])
-
-            ROT_z = Matrix([[ cos(y), -sin(y), 0],
-                            [ sin(y),  cos(y), 0],
-                            [      0,       0, 1]])
-
-            ROT_EE = ROT_z * ROT_y * ROT_x
-
-            #Allign DH with URDF file
-            rot_err = ROT_z.subs(y,radians(180)) * ROT_y.subs(p,radians(-90))
-
-            #End-Effector orientation Matrix in DH frame
-            ROT_EE = ROT_EE * rot_err
-            rot_ee = ROT_EE.subs({'r':roll ,'p':pitch ,'y':yaw})
-            
-            #Extract euler angles from rotation matrix
-            rr = atan2(R3_6[2,2], -R3_6[0,2])
-            rp = atan2(sqrt(R3_6[0,2]*R3_6[0,2] + R3_6[2,2]*R3_6[2,2]),R3_6[1,2])
-            ry = atan2(-R3_6[1,1],R3_6[1,0])
-
-            #End-Effector pose vector
-            pos_ee = Matrix([[px],
-                             [py],
-                             [pz]])
-
-            #TODO: JT Algorithm
+                if (np.sum(np.abs(delta_x)) >= 10):
+                    break
 
             #Populate response
-            joint_trajectory_point.positions = [theta1,theta2,theta3,theta4,theta5,theta6]
+            joint_trajectory_point.positions = [q[0]q[1],q[2],q[3],q[4],q[5]]
             joint_trajectory_list.append(joint_trajectory_point)
 
     return CalculateIKResponse(joint_trajectory_list)
