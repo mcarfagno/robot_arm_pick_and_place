89514927a199ddc38c208047f3a2e14c4fbeede3
mcarfagno <m.carfagno@outlook.it>
2019-08-14 11:28:03 +0100

    JointTrajectory action client example

--- ur5_pick_place/ur5_arm/scripts/joint_trajectory_client.py
+++ ur5_pick_place/ur5_arm/scripts/joint_trajectory_client.py
@@ -2,11 +2,13 @@
 
 import rospy
 import actionlib
+import math
 
 from control_msgs.msg import (
     FollowJointTrajectoryAction,
     FollowJointTrajectoryGoal,
 )
+
 from trajectory_msgs.msg import (
     JointTrajectoryPoint,
 )
@@ -14,59 +16,113 @@ from trajectory_msgs.msg import (
 from copy import copy
 
 class Trajectory(object):
-   def __init__(self, ns):
-       self._client = actionlib.SimpleActionClient(
-           ns + "/" + "follow_joint_trajectory",
-           FollowJointTrajectoryAction,
-       )
-       self._goal = FollowJointTrajectoryGoal()
-       server_up = self._client.wait_for_server(timeout=rospy.Duration(10.0))
-       if not server_up:
-           rospy.logerr("Timed out waiting for Joint Trajectory"
+    def __init__(self, ns, joint_names):
+        
+        self._ns = ns
+        self._joint_names = joint_names
+
+        # create action client
+        self._client = actionlib.SimpleActionClient(
+           self._ns + "/" + "follow_joint_trajectory",
+           FollowJointTrajectoryAction)
+
+        # verify joint trajectory action server is available
+        server_up = self._client.wait_for_server(timeout=rospy.Duration(10.0))
+        if not server_up:
+            rospy.logerr("Timed out waiting for Joint Trajectory"
                          " Action Server to connect. Start the action server"
                          " before running example.")
-           rospy.signal_shutdown("Timed out waiting for Action Server")
-           sys.exit(1)
+            rospy.signal_shutdown("Timed out waiting for Action Server")
+            sys.exit(1)
+
+        # create request
+        self._goal = FollowJointTrajectoryGoal()
 
     def add_point(self, positions, time):
-       point = JointTrajectoryPoint()
+        '''
+        Adds a waypoint to trajectory.
+        '''
+        point = JointTrajectoryPoint()
         point.positions = copy(positions)
         point.time_from_start = rospy.Duration(time)
         self._goal.trajectory.points.append(point)
 
     def start(self):
+        '''
+        Sends FollowJointTrajectoryAction request to action server.
+        '''
+        self._goal.trajectory.joint_names = self._joint_names
         self._goal.trajectory.header.stamp = rospy.Time.now()
         self._client.send_goal(self._goal,feedback_cb=self._feedback)
 
     def stop(self):
-        self._client.cancel_goal()
+        '''
+        Preempts trajectory execution.
+        '''
+        if (self._client.gh is not None and
+            self._client.get_state() == actionlib.GoalStatus.ACTIVE):
+            self._client.cancel_goal()
 
-    def wait(self, timeout=15.0):
-        self._client.wait_for_result(timeout=rospy.Duration(timeout))
+    def wait(self, timeout=10.0):
+        '''
+        Waits for and verifies trajectory execution result.
+        '''
+        finish = self._client.wait_for_result(timeout=rospy.Duration(timeout))
+        result = (self._client.get_result().error_code == 0)
+
+        if all([finish, result]):
+            return True
+        else:
+            msg = ("Trajectory action failed or did not finish before "
+                   "timeout/interrupt.")
+            rospy.logwarn(msg)
+            return False
 
     def result(self):
+        '''
+        Returns the trajectory execution result.
+        '''
         return self._client.get_result()
 
     def _feedback(self,data):
-        print(data.actual.time_from_start)
+        '''
+        Feedback callback
+        '''
+        print(data)
 
 def main():
+    print("Initializing node... ")
+    rospy.init_node("follow_joint_trajectory_node",anonymous=True)
+    print("Running. Ctrl-c to quit")
 
-    traj = Trajectory("arm_controller")
+    joints_names_list=["shoulder_pan_joint",
+                    "shoulder_lift_joint",
+                    "elbow_joint",
+                    "wrist_1_joint",
+                    "wrist_2_joint",
+                    "wrist_3_joint"]
+
+    traj = Trajectory(ns="arm_controller",joint_names=joints_names_list)
     rospy.on_shutdown(traj.stop)
 
-    # Command Current Joint Positions first
-    limb_interface = baxter_interface.limb.Limb(limb)
-    current_angles = [limb_interface.joint_angle(joint) for joint in limb_interface.joint_names()]
-    traj.add_point(current_angles, 0.0)
+    # First Trajectory W.P.
+    traj.add_point([math.radians(30),
+                    math.radians(-120),
+                    math.radians(120),
+                    0,
+                    math.radians(90),
+                    0], 3.0)
 
-    p1 = positions[limb]
-    traj.add_point(p1, 7.0)
-    traj.add_point([x * 0.75 for x in p1], 9.0)
-    traj.add_point([x * 1.25 for x in p1], 12.0)
+    # Second Trajectory W.P.
+    traj.add_point([math.radians(90),
+                    math.radians(-120),
+                    math.radians(120),
+                    math.radians(90),
+                    math.radians(90),
+                    0], 6.0)
     traj.start()
     traj.wait(15.0)
     print("Exiting - Joint Trajectory Action Test Complete")
 
 if __name__ == "__main__":
-    smain()
+    main()
